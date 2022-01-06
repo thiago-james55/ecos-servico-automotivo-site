81389fc9eda5ab7fe97c4d8ddeade728193d421a
Thiago Gonçalves Souza <tsyhwh>
2022-01-06 00:05:09 -0300

    mais algumas implementações , adiconando um pequeno script que muda o icone ao dar hover nos nav menu, termino parcial da redação das paginas e alguns detalhes de design

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,15 +1,7 @@
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const ecos = { lat: -23.5393972, lng: -46.4507823 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: ecos,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: ecos,
-      map: map,
-    });
-  }
+function mudaFoto (path) {
+
+  path = 'imagens/' + path + '.png';
+
+  document.getElementById("icone").src = path;
+
+}
